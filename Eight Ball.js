00eb30e067b36ecfb39d633ece93277cbd261ca2
AndyGames21 <77165906+AndyGames21@users.noreply.github.com>
2022-10-21 21:43:01 +0100

    Added User Name And Question

---
+++ Eight Ball.js
@@ -0,0 +1,5 @@
+//Variables
+let userName  = 'Andy'
+const userQuestion = 'Will I Be Rich?'
+//What To Do if userName is inputed
+userName ? console.log(`Hello, ${userName}!`) : console.log('Hello!')
