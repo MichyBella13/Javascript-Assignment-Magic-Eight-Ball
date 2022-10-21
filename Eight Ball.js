d6708ad786f608b7a2ea72fb42093ff0286a35de
AndyGames21 <77165906+AndyGames21@users.noreply.github.com>
2022-10-21 21:49:12 +0100

    Variables Finished And User Question Added To Console.log

--- Eight Ball.js
+++ Eight Ball.js
@@ -1,5 +1,9 @@
 //Variables
 let userName  = 'Andy'
 const userQuestion = 'Will I Be Rich?'
+let randomNumber = Math.floor(Math.random() * 8)
+let eightBall = ' '
 //What To Do if userName is inputed
 userName ? console.log(`Hello, ${userName}!`) : console.log('Hello!')
+//Console Log of question asked
+console.log(`${userName} aske: ${userQuestion}`)
