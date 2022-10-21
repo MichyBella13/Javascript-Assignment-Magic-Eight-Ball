dc9f5bc0d250dac27e734e3c0d03c5a72f61d70f
AndyGames21 <77165906+AndyGames21@users.noreply.github.com>
2022-10-21 21:56:16 +0100

    All statements added

--- Eight Ball.js
+++ Eight Ball.js
@@ -7,3 +7,30 @@ let eightBall = ' '
 userName ? console.log(`Hello, ${userName}!`) : console.log('Hello!')
 //Console Log of question asked
 console.log(`${userName} aske: ${userQuestion}`)
+//Statements
+switch (randomNumber) {
+case 0 :
+		eightBall = 'It is certain'
+		break;
+case 1 :
+		eightBall = 'It is decidedly so'
+		break;
+case 2 :
+		eightBall = 'Reply hazy try again'
+		break;
+case 3 :
+		eightBall = 'Cannot predict now'
+		break;
+case 4 :
+		eightBall = 'Do not count on it'
+		break;
+case 5 :
+		eightBall = 'My sources say no'
+		break;
+case 6 :
+		eightBall = 'Outlook not so good'
+		break;
+case 7 :
+		eightBall = 'Signs point tto yes'
+		break;
+
